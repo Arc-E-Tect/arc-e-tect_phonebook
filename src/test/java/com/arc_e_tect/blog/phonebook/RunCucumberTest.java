32013f5bbd48048b397f63bad0664288d6bfd388
Iwan Eising <iwan@i-ware.net>
2020-03-02 00:08:42 +0100

    Initial Commit

---
+++ src/test/java/com/arc_e_tect/blog/phonebook/RunCucumberTest.java
@@ -0,0 +1,28 @@
+package com.arc_e_tect.blog.phonebook;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class RunCucumberTest {
+
+    @Given("some initial state")
+    public void some_initial_state() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("this is a trigger for certain behavior")
+    public void this_is_a_trigger_for_certain_behavior() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("certain behavior is exposed")
+    public void certain_behavior_is_exposed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/com/arc_e_tect/blog/phonebook/app.feature
@@ -0,0 +1,7 @@
+Feature: application - Greetings
+
+  Scenario: application - 01 - Greeting Arc-E-Tect
+    Given some initial state
+    When this is a trigger for certain behavior
+    Then certain behavior is exposed
+
