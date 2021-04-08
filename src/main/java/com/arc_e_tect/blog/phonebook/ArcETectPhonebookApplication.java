04ebf56b9eb27e949069d671961a166b7a0a337e
Iwan Eising <iwan@i-ware.net>
2021-04-09 00:51:03 +0200

    Initial Commit

---
+++ src/main/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplication.java
@@ -0,0 +1,13 @@
+package com.arc_e_tect.blog.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcETectPhonebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcETectPhonebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arc_e_tect.blog.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcETectPhonebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
