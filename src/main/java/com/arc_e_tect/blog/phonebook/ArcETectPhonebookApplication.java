34830b90a94df304f4279d3ba3b634be07a0ec29
Iwan Eising <iwan@i-ware.net>
2021-04-17 17:58:56 +0200

    barebones part 1 - mock'ed completely, removed all obsolete stuff

--- src/main/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplication.java
+++ src/main/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplication.java
@@ -1,7 +1,6 @@
 package com.arc_e_tect.blog.phonebook;
 
 import org.mockserver.integration.ClientAndServer;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
--- src/main/java/com/arc_e_tect/blog/phonebook/model/Contact.java
+++
@@ -1,9 +0,0 @@
-package com.arc_e_tect.blog.phonebook.model;
-
-import lombok.Data;
-import lombok.NonNull;
-
-@Data
-public class Contact {
-    @NonNull private final String phoneNumber;
-}
