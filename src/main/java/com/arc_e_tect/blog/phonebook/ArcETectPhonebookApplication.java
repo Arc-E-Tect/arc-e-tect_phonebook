73931853dda82e73016c551054f57dc281d49933
Arc-E-Tect <ieising@hotmail.com>
2021-07-14 11:43:08 +0200

    feat: switch from mock to real

--- src/main/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplication.java
+++ src/main/java/com/arc_e_tect/blog/phonebook/ArcETectPhonebookApplication.java
@@ -1,50 +1,12 @@
 package com.arc_e_tect.blog.phonebook;
 
-import org.mockserver.integration.ClientAndServer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.mockserver.integration.ClientAndServer.startClientAndServer;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.arc_e_tect.blog"})
 public class ArcETectPhonebookApplication {
 
-    private static ClientAndServer mockServer = startClientAndServer(9090);
-
 	public static void main(String[] args) {
-		String bodyContacts = "[ { \"id\": 1, \"name\": \"Arc-E-Tec\", \"phone\": 55521212346 }," +
-				"{ \"id\": 2, \"name\": \"Three Axis\", \"phone\": 55521467754 }," +
-				"{ \"id\": 3, \"name\": \"E-One\",\"phone\": 55523173566 } ]";
-		String[] bodyContact = {
-				"{\"id\":1,\"name\":\"Arc-E-Tec\",\"phone\":55521212346}"
-				,"{\"id\":2,\"name\":\"Three Axis\",\"phone\":55521467754}"
-				,"{\"id\":3,\"name\":\"E-One\",\"phone\":55523173566}"
-		};
-		mockServer.when(request().withMethod("GET")
-				.withPath("/contacts"))
-				.respond(response().withStatusCode(200)
-						.withHeader("Content-Type", "application/hal+json")
-						.withBody(bodyContacts));
-		mockServer.when(request().withMethod("GET")
-				.withPath("/contacts/1"))
-				.respond(response().withStatusCode(200)
-						.withHeader("Content-Type", "application/hal+json")
-						.withBody(bodyContact[0]));
-		mockServer.when(request().withMethod("GET")
-				.withPath("/contacts/2"))
-				.respond(response().withStatusCode(200)
-						.withHeader("Content-Type", "application/hal+json")
-						.withBody(bodyContact[1]));
-		mockServer.when(request().withMethod("GET")
-				.withPath("/contacts/3"))
-				.respond(response().withStatusCode(200)
-						.withHeader("Content-Type", "application/hal+json")
-						.withBody(bodyContact[2]));
-		mockServer.when(request().withMethod("GET")
-				.withPath("/contacts/.*"))
-				.respond(response().withStatusCode(404)
-						.withHeader("Content-Type", "application/hal+json")
-						.withBody("{\"error\" : \"Contact not found\"}"));
+		SpringApplication.run(ArcETectPhonebookApplication.class, args);
 	}
 }
---
+++ src/main/java/com/arc_e_tect/blog/phonebook/web/index/IndexController.java
@@ -0,0 +1,21 @@
+package com.arc_e_tect.blog.phonebook.web.index;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public RepresentationModel index() {
+        RepresentationModel index = new RepresentationModel();
+        index.add(linkTo(IndexController.class).withSelfRel());
+        return index;
+    }
+
+}
