ac2a8aafef56686464b131e6c7ccf33fd34a6d88
Arc-E-Tect <ieising@hotmail.com>
2022-03-02 15:00:56 +0100

    upgrade to Cucumber 7

--- src/bdd-test/java/com/arc_e_tect/blog/phonebook/RunApiCucumberTest.java
+++ src/bdd-test/java/com/arc_e_tect/blog/phonebook/RunApiCucumberTest.java
@@ -1,7 +1,20 @@
 package com.arc_e_tect.blog.phonebook;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/arc_e_tect/blog/phonebook")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty," +
+        "json:build/reports/bdd-test/Cucumber.json," +
+        "junit:build/reports/bdd-test/Cucumber.xml," +
+        "html:build/reports/bdd-test/Cucumber.html")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore and not @wip")
 public class RunApiCucumberTest {
 }
