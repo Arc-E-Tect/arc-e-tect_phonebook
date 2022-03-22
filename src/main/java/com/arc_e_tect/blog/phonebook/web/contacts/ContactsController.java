72475f94282fcf3d782cea28d0b58efdde05b0b4
Arc-E-Tect <ieising@hotmail.com>
2022-03-22 21:52:53 +0100

    implement API to retrieve all Contacts when there are none Satisfy scenario 01 from Feature Get all contacts available in the phonebook

--- src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
+++ src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
@@ -1,27 +1,37 @@
 package com.arc_e_tect.blog.phonebook.web.contacts;
 
-import com.arc_e_tect.blog.phonebook.web.index.IndexController;
-import org.springframework.hateoas.RepresentationModel;
+import com.arc_e_tect.blog.phonebook.domain.Contact;
+import com.arc_e_tect.blog.phonebook.resource.ContactResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value= {"/contacts"})
 public class ContactsController {
     HashMap<String, String> contacts;
+    private final ContactsResourceAssembler resourceAssembler;
+
+    @Autowired
+    ContactsController(ContactsResourceAssembler resourceAssembler) {
+        this.resourceAssembler = resourceAssembler;
+    }
 
     @GetMapping(produces = {"application/hal+json", MediaType.APPLICATION_JSON_VALUE})
-    public RepresentationModel contacts() {
-        RepresentationModel contactResources = new RepresentationModel();
-        contactResources.add(linkTo(IndexController.class).withSelfRel());
-        contactResources.add(linkTo(ContactsController.class).withRel("contacts"));
-        return contactResources;
+    public CollectionModel<ContactResource> getAllContacts(HttpServletResponse response) {
+        List<Contact> contactList = new LinkedList<>();
+
+        CollectionModel<ContactResource> result = resourceAssembler.toCollectionModel(contactList);
+
+        return result;
     }
 
 }
