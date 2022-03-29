ef0712335901539ea68140d9b0b794bcc7002d30
Arc-E-Tect <ieising@hotmail.com>
2022-03-29 21:08:49 +0200

    replace documentation with bogus documentation

--- src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
+++ src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
@@ -2,6 +2,11 @@ package com.arc_e_tect.blog.phonebook.web.contacts;
 
 import com.arc_e_tect.blog.phonebook.domain.Contact;
 import com.arc_e_tect.blog.phonebook.resource.ContactResource;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
@@ -25,6 +30,13 @@ public class ContactsController {
         this.resourceAssembler = resourceAssembler;
     }
 
+    @Operation(summary = "Get all micro plastics from the Atlantic.",
+            description = "# Plastic Man\n\nGet all **spider legs** stored in the _closet_.\n\nPlastic Men have four limbs: a left and a right arm and a left and right leg.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "OK", description = "200",
+                    content = { @Content(mediaType = "application/johnson",
+                            schema = @Schema(implementation = String.class)) }),
+            })
     @GetMapping(produces = {"application/hal+json", MediaType.APPLICATION_JSON_VALUE})
     public CollectionModel<ContactResource> getAllContacts(HttpServletResponse response) {
         List<Contact> contactList = new LinkedList<>();
