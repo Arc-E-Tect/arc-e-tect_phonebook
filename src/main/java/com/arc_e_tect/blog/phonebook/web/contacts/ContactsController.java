e739debc4b9e6edc9d000eda2a44c42092d50f85
Arc-E-Tect <ieising@hotmail.com>
2022-04-16 00:30:35 +0200

    remove annotations related to OpenAPI We will use Asciidoctor and RestDocs to generate the OpenAPI spec

--- src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
+++ src/main/java/com/arc_e_tect/blog/phonebook/web/contacts/ContactsController.java
@@ -2,11 +2,6 @@ package com.arc_e_tect.blog.phonebook.web.contacts;
 
 import com.arc_e_tect.blog.phonebook.domain.Contact;
 import com.arc_e_tect.blog.phonebook.resource.ContactResource;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
@@ -30,13 +25,6 @@ public class ContactsController {
         this.resourceAssembler = resourceAssembler;
     }
 
-    @Operation(summary = "Get all Contacts.",
-            description = "# Contact\n\nGet all **Contacts** stored in the _Arc-E-Tect_ Phonebook.\n\nContacts have three fields: an id, which is a number, a name, which is the fullname of the Contact and a phone-number.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Contact.class)) }),
-            })
     @GetMapping(produces = {"application/hal+json", MediaType.APPLICATION_JSON_VALUE})
     public CollectionModel<ContactResource> getAllContacts(HttpServletResponse response) {
         List<Contact> contactList = new LinkedList<>();
