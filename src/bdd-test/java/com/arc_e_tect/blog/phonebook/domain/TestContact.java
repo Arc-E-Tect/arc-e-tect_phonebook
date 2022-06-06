759055872ae28025d31d3c0e6a0b93c78f9045c8
Arc-E-Tect <ieising@hotmail.com>
2022-06-06 02:47:19 +0200

    make TestContact extend Contact By extending TestContact from Contact, it is ensured that all changes to Contact are reflected in TestContact, thus reducing the chance of errors

--- src/bdd-test/java/com/arc_e_tect/blog/phonebook/domain/TestContact.java
+++ src/bdd-test/java/com/arc_e_tect/blog/phonebook/domain/TestContact.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 @Getter @Setter @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper=false)
-@NoArgsConstructor @AllArgsConstructor
-public class TestContact {
-    private long id;
-    private String name;
-    private String phone;
+@NoArgsConstructor
+public class TestContact extends Contact {
 }
